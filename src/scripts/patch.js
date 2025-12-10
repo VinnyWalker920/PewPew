45de6fe4e4499c4764573051f46ff30463ca1695
zappa <vinnywalker920@gmail.com>
2025-12-09 22:35:39 -0600

    Patch just sucks

--- src/scripts/patch.js
+++ src/scripts/patch.js
@@ -1,41 +1,52 @@
-Ext.define("pve-qemu-power-summary", {
-  override: "PVE.qemu.Summary",
-  initComponent: function () {
-    let me = this;
-    me.callParent();
+Ext.ClassManager.onCreated('PVE.qemu.Summary', function() {
+    Ext.define("pve-qemu-power-summary", {
+        override: "PVE.qemu.Summary",
 
-    // add custom row after VM info loads
-    me.on("afterrender", function () {
-      let vmid = me.pveSelNode.data.vmid;
+        initComponent: function() {
+            let me = this;
+            me.callParent();
 
-      Proxmox.Utils.API2Request({
-        url: "/nodes/" + Proxmox.NodeName + "/qemu/" + vmid + "/status/current",
-        method: "GET",
-        waitMsgTarget: me,
-        success: function () {
-          Proxmox.Utils.run_command(
-            ["/usr/local/bin/powerquery.sh", vmid],
-            function (response) {
-              try {
-                let data = JSON.parse(response);
-                let watts = data.watts + " W";
-                let kwh = data.kwh + " kWh";
+            console.log("Power patch active for VM:", me.pveSelNode.data.vmid);
 
-                // append to Summary Info box
-                me.down("#info").add({
-                  xtype: "box",
-                  html:
-                    '<div style="padding:2px 0;padding-left:6px;"><b>Power:</b> ' +
-                    watts +
-                    "  |  <b>KWH:</b> " +
-                    kwh +
-                    "</div>",
+            // after the summary box renders
+            me.on("afterrender", function() {
+                let vmid = me.pveSelNode.data.vmid;
+
+                Proxmox.Utils.API2Request({
+                    url: "/nodes/" + Proxmox.NodeName + "/qemu/" + vmid + "/status/current",
+                    method: "GET",
+                    waitMsgTarget: me,
+                    success: function() {
+                        Proxmox.Utils.run_command(
+                            ["/usr/local/bin/powerquery.sh", vmid],
+                            function(response) {
+                                try {
+                                    let data = JSON.parse(response);
+                                    let watts = data.watts + " W";
+                                    let kwh = data.kwh + " kWh";
+
+                                    // append to Summary Info box
+                                    me.down("#info").add({
+                                        xtype: "box",
+                                        html: '<div style="padding:2px 0;padding-left:6px;">' +
+                                              '<b>Power:</b> ' + watts + 
+                                              '  |  <b>KWH:</b> ' + kwh +
+                                              '</div>'
+                                    });
+
+                                    console.log("Power info added for VM:", vmid);
+                                } catch (e) {
+                                    console.warn("Power patch JSON parse error:", e);
+                                }
+                            }
+                        );
+                    },
+                    failure: function(response) {
+                        console.warn("Power patch API request failed:", response);
+                    }
                 });
-              } catch (e) {}
-            }
-          );
-        },
-      });
+            });
+        }
     });
-  },
+    console.log("Patch defined: pve-qemu-power-summary");
 });
