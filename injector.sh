8e4cff8bf9594aa9e8b5a8329b44ae37e9abaa4c
zappa <vinnywalker920@gmail.com>
2025-12-09 22:20:32 -0600

    Some pathing changes

--- injector.sh
+++ injector.sh
@@ -2,13 +2,13 @@
 set -e
 
 PATCH_SRC="./src/scripts/patch.js"
-PATCH_DST="/usr/share/pve-manager/ext4/patch.js"
+PATCH_DST="/usr/share/pve-manager/js/patch.js"
 TPL="/usr/share/pve-manager/index.html.tpl"
 TPL_BAK="/usr/share/pve-manager/index.html.tpl.bak"
-INJECTION='<script type="text/javascript" src="/pve2/ext4/patch.js"></script>'
+INJECTION='<script type="text/javascript" src="/pve2/js/patch.js"></script>'
 
-echo "Creating folder..."
-mkdir -p /usr/share/pve-manager/ext4/
+echo "Creating js folder..."
+mkdir -p /usr/share/pve-manager/js
 
 echo "Copying patch..."
 cp "$PATCH_SRC" "$PATCH_DST"
